5b81d5993c1321779e5d35990c376fc7a43ca1ba
Christopher Burge and Jacqueline Remmel <jacqueline@>
2019-09-09 10:18:07 -0700

    Add chicken constructor with methods for hunger and eggs.

--- spec/animal-game-spec.js
+++ spec/animal-game-spec.js
@@ -1,4 +1,4 @@
-import { Fish } from '../src/animal-game.js';
+import { Fish, Chicken } from '../src/animal-game.js';
 
 describe ('Hungry Fish', function() {
   let fish = new Fish();
@@ -15,8 +15,32 @@ describe ('Hungry Fish', function() {
     expect(fish.hunger).toEqual(0);
   });
 
-  it('should have a hunger level of 1 after 2001 milliseconds', function() {
+  it('should have a hunger level of 1 after 2 seconds', function() {
     jasmine.clock().tick(2001);
     expect(fish.hunger).toEqual(1);
+  });
+});
+
+describe ('Hungry Chicken', function() {
+  let chicken = new Chicken();
+
+  beforeEach(function() {
+    jasmine.clock().install();
+    chicken.setHunger();
+    chicken.setEggs();
+  });
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  });
+
+  it('should have a hunger and egg level of 0 when it is created', function() {
+    expect(chicken.hunger).toEqual(0);
+    expect(chicken.eggs).toEqual(0);
+  });
+
+  it('should have a hunger level of 1 after 2 seconds and an egg level of 2 after 2 seconds', function() {
+    jasmine.clock().tick(2001);
+    expect(chicken.hunger).toEqual(1);
+    expect(chicken.eggs).toEqual(2);
   });
 });
--- src/animal-game.js
+++ src/animal-game.js
@@ -9,3 +9,22 @@ export class Fish {
     }, 2000);
   }
 }
+
+export class Chicken {
+  constructor () {
+    this.hunger = 0;
+    this.eggs = 0;
+  }
+
+  setHunger() {
+    setInterval(() => {
+      this.hunger++;
+    }, 2000);
+  }
+
+  setEggs() {
+    setInterval(() => {
+      this.eggs += 2;
+    }, 2000);
+  }
+}
